3103e2a8fbcf5c1b362d2a3d07eb63e29d0b857c
Unknown <parker.thurston22@gmail.com>
2019-01-23 10:26:37 -0700

    Brackets Code

---
+++ Adventure_Game/game.js
@@ -0,0 +1,19 @@
+// Single Line Comment
+/*
+
+Multiline Comment
+
+*/
+
+// document.write("<h1><em>N<em></h1>");
+
+
+//alert("Warning! Will Robinson... Warning!");
+
+//confirm("Do you like pokemon?");
+
+//prompt("What type of pokemon do you like?");
+
+var pc = prompt("What is your name?");
+
+confirm("So your name is "+pc+"?");
